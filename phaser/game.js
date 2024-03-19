37a78741ce54593a362ebd5faa28d7bb300eaa84
DarkSystemGit <85317259+DarkSystemGit@users.noreply.github.com>
2024-03-19 13:43:52 +0000

    loaders

--- phaser/game.js
+++ phaser/game.js
@@ -15,12 +15,17 @@ function loadBackground(phaser, name) {
 }
 
 function loadMap(phaser, map, tiles) {
-    phaser.load.tilemapCSV("map", `assets/${map}.csv`);
-    const map = phaser.make.tilemap({
-        key: "map",
+    var mapData={
         tileWidth: 16,
         tileHeight: 16
-    });
+    }
+    if(map.type=='csv'){
+    phaser.load.tilemapCSV("map", `assets/${map.name}.csv`);
+    mapData.key='map'
+    }else{
+        mapData.data=map.level
+    }
+    const map = phaser.make.tilemap(mapData);
     map.createLayer(0, map.addTilesetImage(tiles), 0, 0);
 }
 
@@ -29,13 +34,21 @@ function loadScene(scene) {
         type: Phaser.AUTO,
         width: window.innerWidth,
         height: window.innerHeight,
+        pixelArt: true,
         scene: {
             preload: scene.onStart,
             create: scene.onCreate,
             update: scene.onFrame
-        }
+        },
+        physics: {
+            default: "arcade",
+            arcade: {
+              gravity: { y: 0 } // Top down game, so no gravity
+            }
+          }
     }
 }
+var player;
 const main = {
     onStart: () => {},
     onCreate: () => {},
