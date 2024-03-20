b689825fc2480e34f00cf7ae8311b55b2a9dc42d
Your Name <you@example.com>
2024-03-20 17:01:11 -0400

    maps!

--- phaser/game.js
+++ phaser/game.js
@@ -1,33 +1,21 @@
 function load(phaser, assets) {
     assets.forEach(asset => {
-        if (asset[1]) {
+        switch (asset[1]) {
+            case 'ss':
             phaser.load.spritesheet(asset[0], `assets/${asset[0]}.png`, {
                 frameWidth: asset[1],
                 frameHeight: asset[2]
             })
+            case 'img':
+            phaser.load.image(asset[0], `assets/${asset[0]}.png`)
+            case 'map':
+            phaser.load.tilemapTiledJSON(asset[0],`assets/maps/${asset[0]}.json`)
         }
-        phaser.load.image(asset[0], `assets/${asset[0]}.png`)
+        
     });
 }
 
-function loadBackground(phaser, name) {
-    phaser.add.image(0, 0, name).setOrigin(0, 0)
-}
 
-function loadMap(phaser, map, tiles) {
-    var mapData={
-        tileWidth: 16,
-        tileHeight: 16
-    }
-    if(map.type=='csv'){
-    phaser.load.tilemapCSV("map", `assets/${map.name}.csv`);
-    mapData.key='map'
-    }else{
-        mapData.data=map.level
-    }
-    const map = phaser.make.tilemap(mapData);
-    map.createLayer(0, map.addTilesetImage(tiles), 0, 0);
-}
 
 function loadScene(scene) {
     return {
@@ -50,9 +38,32 @@ function loadScene(scene) {
 }
 var player;
 const main = {
-    onStart: () => {},
-    onCreate: () => {},
-    onFrame: () => {}
+    onStart: () => {
+        load(this,[['main','map'],['citytiles','img'],['schoolassets','img']])
+    },
+    onCreate: () => {
+        const map = this.make.tilemap({ key: "main" });
+        const citytileset = map.addTilesetImage("schoolassets", "schoolassets");
+        const itemstileset = map.addTilesetImage("citytiles", "citytiles");
+        map.createLayer("Ground", [citytileset,itemstileset], 0, 0);
+        map.createLayer("Objects", [citytileset,itemstileset], 0, 0);
+        map.createLayer("Collision", [citytileset,itemstileset], 0, 0);
+        const cursors = this.input.keyboard.createCursorKeys();
+        controls = new Phaser.Cameras.Controls.FixedKeyControl({
+          camera: camera,
+          left: cursors.left,
+          right: cursors.right,
+          up: cursors.up,
+          down: cursors.down,
+          speed: 0.5
+        });
+      
+        // Constrain the camera so that it isn't allowed to move outside the width/height of tilemap
+        camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    },
+    onFrame: (time,delta) => {
+        controls.update(delta);
+    }
 }
 
-const game = new Phaser.Game();
+const game = new Phaser.Game(loadScene(main));
