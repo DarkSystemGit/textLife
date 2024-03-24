0106829dd1457c549594565455e0218cdf27d6c5
Your Name <you@example.com>
2024-03-24 19:58:00 -0400

    save the children

--- phaser/game.js
+++ phaser/game.js
@@ -1,4 +1,4 @@
-function rand(max,min){
+function rand(max, min) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 function load(phaser, assets) {
@@ -41,21 +41,24 @@ function loadScene(scene) {
         }
     }
 }
-var hiding=false
-var attack=false
-var win=0
+var hiding = false
+var attack = false
+var win = 0
 var hour = [12, 0]
+var hattack = rand(50, 10)
+var emo = rand(5000, 1000)
 var player;
 var hKey;
 var children;
-var hattack=rand(50,10)
+var map;
+
 const main = {
     onStart: function () {
 
         load(this, [['main', 'map'], ['citytiles', 'img'], ['ground', 'img'], ['mask', 'img'], ['emotes', 'img'], ['chars', 'img'], ['schoolassets', 'img'], ['player', 'ss']])
     },
     onCreate: function () {
-        const map = this.make.tilemap({ key: "main" });
+        map = this.make.tilemap({ key: "main" });
         const sp = map.findObject("data", obj => obj.name === "Spawn Point");
         player = this.player = this.physics.add.sprite(sp.x, sp.y, 'player')
         //player.frame=6
@@ -93,7 +96,7 @@ const main = {
         var coll = map.createLayer("Collision", ['citytiles', 'schoolassets', 'ground', 'chars'], 0, 0);
         var cl = map.createLayer("cleanup", ['citytiles', 'schoolassets', 'ground', 'chars'], 0, 0);
         var cl2 = map.createLayer("cleanup2", ['citytiles', 'schoolassets', 'ground', 'chars'], 0, 0);
-       
+
         coll.setCollisionByExclusion([-1])
         cl.setCollisionByProperty({ col: true })
         //cl2.setCollisionByExclusion([-1])
@@ -121,18 +124,27 @@ const main = {
         }, true)
 
         // fill it with black
-        hKey=this.input.keyboard.addKey('H')
-        
+        hKey = this.input.keyboard.addKey('H')
+        sKey = this.input.keyboard.addKey(this.input.keyboard.keyCodes.SPACE)
         rt.setDepth(14)
         player.setDepth(13)
         console.log(map)
     },
     onFrame: function (t, delta) {
-        if(win!=0){return}
-        if(hKey.isDown)hiding=!hiding
+        if (win != 0) { return }
+        if (hKey.isDown) hiding = !hiding
         //console.log(getTime())
-        
-       document.getElementsByClassName('text')[0].innerText=getTime()
+        if (sKey.getDuration() ==emo){
+        var closest=map.findObject("data", (obj) => {
+            if(obj.name === "h"&&getDistance(player.x,player.y,obj.x,obj.y)<=2){
+                return obj
+            }
+        });
+        emo = rand(5000, 1000)
+        map.removeTile(closest)
+        children++
+        }
+        document.getElementsByClassName('text')[0].innerText = getTime()
 
         const speed = 175;
         const prevVelocity = player.body.velocity.clone();
@@ -162,16 +174,16 @@ const main = {
         this.rt.clear();
         this.rt.fill(0x000000, 1)
         this.rt.setTint(0x0a2948)
-        
-        if(!hiding)this.rt.erase('mask', this.player.x - 53, this.player.y - 53)
-        if(!hiding&&attack){win=-1;loss()}
-        if(hour[0]==6&&children==7){win=1;winGame()}
-        if(hour[0]==6&&hour[1]==5&&win==0){win=-1;loss()}
-        document.getElementById('hr').innerText=`${getTime().split(':')[0]} AM`
-        document.getElementById('rtime').innerText=`${Math.abs(6-hour[0])} hours remaining`
+
+        if (!hiding) this.rt.erase('mask', this.player.x - 53, this.player.y - 53)
+        if (!hiding && attack) { win = -1; loss() }
+        if (hour[0] == 6 && children == 7) { win = 1; winGame() }
+        if (hour[0] == 6 && hour[1] == 5 && win == 0) { win = -1; loss() }
+        document.getElementById('hr').innerText = `${getTime().split(':')[0]} AM`
+        document.getElementById('rtime').innerText = `${Math.abs(6 - hour[0])} hours remaining`
 
 
-        
+
         /*const emotes=this.emotes=this.add.particles(player.x,player.y,{
             frame:['emotes'],
             gravityY: 200,
@@ -181,19 +193,22 @@ const main = {
 }
 function getTime() {
     var res = `${hour[0]}:`
-    if(hour[0]==13){
-        hour[0]=1
+    if (hour[0] == 13) {
+        hour[0] = 1
     }
     if (hour[1] < 10) {
         res = res + '0'
     }
-    
-    if(hour[1]==0){document.getElementsByClassName('center')[0].style.display='block'}
-    if(hour[1]==5){document.getElementsByClassName('center')[0].style.display='none'}
+
+    if (hour[1] == 0) { document.getElementsByClassName('center')[0].style.display = 'block' }
+    if (hour[1] == 5) { document.getElementsByClassName('center')[0].style.display = 'none' }
     return res + hour[1]
 }
+function getDistance(x1, y1, x2, y2) {
+    return Math.hypot(x2 - x1, y2 - y1)
+}
 setInterval(() => {
-    if((hour[0]>=6&&hour[1]>=9&&hour[0]<12)||(hour[1]>hattack+4&&win==-1)){
+    if ((hour[0] >= 6 && hour[1] >= 9 && hour[0] < 12) || (hour[1] > hattack + 4 && win == -1)) {
         document.getElementsByTagName('canvas')[0].remove()
         document.getElementsByClassName('game')[0].remove()
         return
@@ -201,35 +216,35 @@ setInterval(() => {
     if (hour[1] == 60) {
         hour[0]++
         hour[1] = 0
-        hattack=rand(50,10)
+        hattack = rand(50, 10)
     } else {
         hour[1]++
     }
-    if(hour[1]==hattack){
-        attack=true
+    if (hour[1] == hattack) {
+        attack = true
     }
-    if(hour[1]==hattack+2){
-        attack=false
+    if (hour[1] == hattack + 2) {
+        attack = false
     }
     console.log(attack)
 }, 1000)
 
-function loss(){
-    
- document.getElementsByTagName('canvas')[0].classList.add('fadeOut')
- document.getElementsByClassName('game')[0].classList.add('fadeOut')
- var txt=document.getElementsByClassName('endGame')[0]
- txt.innerHTML='  <p>You Failed To Save Them.</p><p>You Shall Now Pay.</p>'
- txt.style.top='40%'
- txt.style.display='block'
- 
-}
-function winGame(){
+function loss() {
+
     document.getElementsByTagName('canvas')[0].classList.add('fadeOut')
     document.getElementsByClassName('game')[0].classList.add('fadeOut')
-    var txt=document.getElementsByClassName('endGame')[0]
-    txt.innerHTML='  <p>You Saved Them.</p><p>He Fled.</p><p>They now may live in peace, due to you.</p><p>Thank You!</p>'
-    txt.style.top='20%'
-    txt.style.display='block'
+    var txt = document.getElementsByClassName('endGame')[0]
+    txt.innerHTML = '  <p>You Failed To Save Them.</p><p>You Shall Now Pay.</p>'
+    txt.style.top = '40%'
+    txt.style.display = 'block'
+
+}
+function winGame() {
+    document.getElementsByTagName('canvas')[0].classList.add('fadeOut')
+    document.getElementsByClassName('game')[0].classList.add('fadeOut')
+    var txt = document.getElementsByClassName('endGame')[0]
+    txt.innerHTML = '  <p>You Saved Them.</p><p>He Fled.</p><p>They now may live in peace, due to you.</p><p>Thank You!</p>'
+    txt.style.top = '20%'
+    txt.style.display = 'block'
 }
 const game = new Phaser.Game(loadScene(main));
